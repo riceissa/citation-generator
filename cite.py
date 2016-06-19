2b0628b3ee1c537946a8e63c1054f646765019e5
Issa Rice <riceissa@gmail.com>
2016-06-18 19:07:37 -0700

    blah blah blah

--- cite.py
+++ cite.py
@@ -2,6 +2,7 @@
 
 import sys
 from bs4 import BeautifulSoup
+import re
 
 def soup2dict(soup, dictionary):
     """
@@ -22,14 +23,20 @@ def soup2dict(soup, dictionary):
             dictionary["publisher"] = tag.get("content").strip()
     if "title" not in dictionary and soup.title is not None:
         dictionary["title"] = soup.title.string.strip()
+    #print(soup.find_all("span", class_="author")[0].contents)
+    #print(soup.find_all("span", class_="date")[0].contents)
+    s = soup.get_text()
+    m = re.search(r'By (\w* \w*)', s)
+    if "author" not in dictionary:
+        dictionary["author"] = m.group(1)
 
-def get_author(soup):
+def get_author(dictionary):
     return "hi"
 
-def get_date(soup):
+def get_date(dictionary):
     pass
 
-def get_title(soup):
+def get_title(dictionary):
     pass
 
 
