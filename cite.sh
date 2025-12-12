e2444c2cf5c44dfcba15a2edc7aaeb555215bca0
Issa Rice <riceissa@gmail.com>
2025-12-12 15:30:07 -0800

    checking in a change from a very long time ago (2016-09-07)

--- cite.sh
+++ cite.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wget --quiet -O - "$1" | python3 cite.py "$1"
+# wget --quiet -O - "$1" | cite.py "$1" | tee /dev/tty | xclip -sel clip
+wget --quiet -O - "$1" | python3 /home/issa/projects/autolink/autolink.py --filetype mediawiki -C "$1" | tee /dev/tty | xclip -sel clip
